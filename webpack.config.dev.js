eef63b836e97aa45009cdd456b226c85b952531e
Carlos Gerardo Perez Horta <carlos.perez@charliegperez.com>
2021-03-24 01:31:23 -0600

    chore(files): change webpack.config.dev.js(add loaders: css-loader and style-loader)

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -34,6 +34,17 @@ module.exports = {
         ],
       },
       {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
         test: /\.(woff|woff2)$/,
         use: {
           loader: 'url-loader',
