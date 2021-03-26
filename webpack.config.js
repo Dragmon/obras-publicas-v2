448ce9b18082e5bba3153d315649258499296d1f
Carlos Gerardo Perez Horta <carlos.perez@charliegperez.com>
2021-03-26 01:09:06 -0600

    chore(files): Change .env.example and webpack.config.js files

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,17 @@ module.exports = {
         ],
       },
       {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
         test: /\.(woff|woff2)$/,
         use: {
           loader: 'url-loader',
