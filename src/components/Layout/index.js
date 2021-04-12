e81a1007d4990b13d215b06a42ae6be501927f22
Carlos Gerardo Perez Horta <carlos.perez@charliegperez.com>
2021-04-12 18:07:17 -0500

    chore(files): Update Layoute

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from '../Header';
-import Footer from '../Footer';
+/* import Header from '../Header';
+import Footer from '../Footer'; */
 
 const index = ({ children }) => (
   <>
-    <Header />
+    {/* <Header /> */}
     {children}
-    <Footer />
+    {/* <Footer /> */}
   </>
 );
 
